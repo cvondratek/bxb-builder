fb1d7cd2c774b68f35e2d1211edba219425290c7
cvondra <cvondra@spaceghost>
2021-05-25 19:12:49 -0700

    run container with host networking

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -t --cpus $(cat /proc/cpuinfo | grep siblings | awk '{print $3}' | tail -1) -u$(id -u) -v$PWD/workdir:/workdir:z -v/dev/shm:/dev/shm:z -v/etc/resolv.conf:/etc/resolv.conf:z bxb-builder:latest
+docker run -t --network=host --cpus $(cat /proc/cpuinfo | grep siblings | awk '{print $3}' | tail -1) -u$(id -u) -v$PWD/workdir:/workdir:z -v/dev/shm:/dev/shm:z -v/etc/resolv.conf:/etc/resolv.conf:z bxb-builder:latest
 
