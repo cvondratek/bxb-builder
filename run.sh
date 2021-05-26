42b2187eeff75a0fc5dfeee962dac61a4377f7d3
cvondra <cvondra@spaceghost>
2021-05-25 18:02:22 -0700

    run container as uid=$(id -u)

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -t --cpus $(cat /proc/cpuinfo | grep siblings | awk '{print $3}' | tail -1) -v$PWD/workdir:/workdir:z -v/dev/shm:/dev/shm:z bxb-builder:latest
+docker run -t --cpus $(cat /proc/cpuinfo | grep siblings | awk '{print $3}' | tail -1) -u$(id -u) -v$PWD/workdir:/workdir:z -v/dev/shm:/dev/shm:z bxb-builder:latest
 
